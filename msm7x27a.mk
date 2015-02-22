b836f1bd2b084675c00083ea09a34828dc0203ba
David Vaz Guijarro <vazdavid@icloud.com>
2015-02-22 03:33:43 +0100

    Remove PRODUCT_AAPT_CONFIG
    A bug caused this to not work properly for hdpi

--- msm7x27a.mk
+++ msm7x27a.mk
@@ -21,9 +21,6 @@ $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/huawei/msm7x27a-common/overlay
 
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-
 CM_BUILDTYPE := NIGHTLY
 
 # Packages
